8b0921143f830294d61d54cd4071909016b728f3
Emily <emily.klemt@web.de>
2023-11-09 23:18:23 +0100

    POLISH: add readme and comments

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Praktikum_1_Primzahlen
-1.Semester/PRP1
+## 1.Semester/PRP1
+
+Name:			Primzahlen
+Beschreibung:	Programm, welches eine Zahl vom Benutzer abfragt und mit dieser folgendes tut:
+				Ist die Zahl gerade oder ungerade 
+				Ist die Zahl eine Primzahl oder keine Primzahl 
+				Alle Primzahlen bis zur eingegebenen Zahl ausgibt
+				Eine Primfaktorzerlegung durchführt
+Autorinnen:	    Emily Klemt, Carolin Altstaedt
+Datum:		    7.11.2023
+Version:	    1
+
+
+Als Primzahlen werden aus der Menge der natürlichen Zahlen N die Zahlen größer als eins
+bezeichnet, die sich nur durch sich selbst und eins ohne Rest teilen lassen. Primzahlen sind ein
+Teilgebiet der elementaren Zahlentheorie. Sie werden z.B. benötigt, um das kleinste gemeinsame
+Vielfache KGV oder den größten gemeinsamen Teiler GGT von zwei Zahlen zu bestimmen.
+
+## Aufgabe 1: Anlegen eines Projekts
+Erstellen Sie ein Projekt mit Namen Primzahlen.
+
+
+## Aufgabe 2: Gerade und ungerade Zahlen
+Fragen Sie vom User eine ganze Zahl vom Typ unsigned int ab. Prüfen Sie danach, ob die Zahl gerade
+oder ungerade ist und geben Sie eine entsprechende Meldung auf dem Bildschirm aus.
+Hinweis: Bei scanf und printf ist %u der Platzhalter für unsigned int.
+
+
+## Aufgabe 3: Auf Primzahl prüfen
+Prüfen Sie für die eingegebene Zahl, ob es sich um eine Primzahl handelt, und geben Sie eine
+entsprechende Meldung aus.
+
+
+## Aufgabe 4: Liste von Primzahlen
+Als nächstes sollen alle Primzahlen bis zur eingegebenen Zahl bestimmt und ausgeben werden. Die
+Liste soll nur erstellt werden, wenn die eingegebene Zahl nicht größer als 1000 ist.
+
+
+## Aufgabe 5: Primfaktoren
+In einem letzten Schritt sollen zu der eingegebenen Zahl alle Primfaktoren ermittelt und ausgegeben
+werden. Mehrfach vorhandene Primfaktoren sollen entsprechend oft ausgegeben werden.
+
+
+## Aufgabe 6: Quellcode gestalten
+Gestalten Sie Ihren Quellcode in einer Form, dass andere Programmierer*innen ihn leicht verstehen:
+* Zu Beginn der Quellcodedatei 5 Zeilen Kommentar mit Name, Beschreibung, Autor, Datum
+und Version
+* Alle angelegten Variablen in separaten Zeilen mit einem kurzen Kommentar rechts daneben
+* Den Rest des Quellcodes in Absätzen mit Kommentaren oberhalb gruppieren
+* Verwenden Sie für alle Bezeichner sinnvolle Namen
+* Benutzen Sie das sog. lower camel case (z.B. eingegebeneZahl)
+* Achten Sie auf korrektes Einrücken
+
+
+## Optionale Zusatzaufgabe
+Fragen Sie vom User zwei natürliche Zahlen ab und ermitteln Sie den größten gemeinsamen Teiler
+mit dem Euklidschen Algorithmus (bitte selbst recherchieren).
--- main.c
+++ main.c
@@ -1,139 +1,173 @@
+ /***
+
+Name:			Primzahlen
+Beschreibung:	Programm, welches eine Zahl vom Benutzer abfragt und mit dieser folgendes tut:
+				Ist die Zahl gerade oder ungerade 
+				Ist die Zahl eine Primzahl oder keine Primzahl 
+				Alle Primzahlen bis zur eingegebenen Zahl ausgibt
+				Eine Primfaktorzerlegung durchführt
+Autorinnen:	    Emily Klemt, Carolin Altstaedt
+Datum:		    7.11.2023
+Version:		1
+
+***/
+
+#define CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdbool.h>
 
 int main() {
+    bool istPrimzahl = true; // Ergebnis der Primzalprüfung wird hier gespeichert, Standard = true
+    bool primzahlListenElement = true; // Liste der Primzahlen wird ausgegeben anhand des Werts dieser Variable
+    unsigned int userEingabeZahl; // Eingabe des Users wird in dieser Variable gespeichert
 
-    // variablen auf true setzen, print nicht in if else
 
-    bool isPrimeNumber = true;
-    bool primeNumberList = true;
-    int userInputNumber;
 
+    //initiale Abfrage einer User-Eingabe (positive Zahl, bestimmt durch %u für unsigned integer),
     printf("Gib eine Zahl (x >= 0) ein: \n");
+    scanf("%u", &userEingabeZahl);  
 
-    scanf("%i", &userInputNumber);  
 
 
-    if(userInputNumber < 0) {
-        printf("Bitte nicht diese Zahl, sie muss x >= 0 entsprechen:");
+    //Sicherheitsabfrage, ob Eingabe wirklich positiv ist, ansonsten wird das programm nicht fortgesetzt.
+    if(userEingabeZahl < 0) {
+        printf("Bitte nicht diese Zahl, sie muss x >= 0 entsprechen.");
+        return 0;
     } else {
-        printf("Deine Zahl ist: %d \n", userInputNumber);
-
-        // ist die Zahl gerade ? userInputNumber%2 = 0?
-        // deine zahl ist gerade
-        // deine zahl ist ungerade
-
-        if(userInputNumber % 2 == 0) {
-            printf("Deine Zahl ist gerade  \n");
-        } else {
-            printf("Deine Zahl ist UNgerade  \n");
-        }
+        //Ausgabe der User-Eingabe
+        printf("Deine Zahl ist: %d \n", userEingabeZahl);
+    } 
 
 
 
-
-
-        if (userInputNumber <= 1) {
-            isPrimeNumber = false; // 1 und 0 sind keine Primzahlen
-        } else {
-            for (int i = 2; i <= userInputNumber/2; i++) { 
-                if (userInputNumber % i == 0) { 
-                    isPrimeNumber = false;
-                    break; // Schleife an dieser Stelle beenden
-                }
-            }
-        }
-
-
-        if(isPrimeNumber == true) {
-            printf("Deine Zahl IST eine Primzahl  \n");
-        } else {
-            printf("Deine Zahl ist KEINE Primzahl  \n");
-        }
+    /*
+    Aufgabe 2: 
+    Prüfen ob Eingabe eine gerade oder ungerade Zahl ist mittels %-Modulo
+    Ausgabe des Ergebnisses der Prüfung
+    */
+    if(userEingabeZahl % 2 == 0) {
+        printf("Deine Zahl ist gerade  \n");
+    } else {
+        printf("Deine Zahl ist UNgerade  \n");
+    }
 
 
 
-
-
-
-
-        if(userInputNumber > 1000) {
-            printf("Zahl zu gr0ß, sry!  \n");
-        } else {
-            printf("Primzahlen bis zu deiner Zahl:\n");
-            // schleife beginnt bei 1, da 0 keine Primzahl ist, und somit ausgeschlossen werden kann
-            for (int i = 2; i < userInputNumber; i++) { 
-
-                // allePrimzahlen;
-                // allePrimzahlen = "1,3,7"
-                // printf("%d", i);
-
-                for (int a = 2; a <= i/2; a++) { 
-  
-                    if (i % a == 0) { 
-                        primeNumberList = false; 
-                        break; 
-                    } else {
-                        primeNumberList = true; 
-                    }
-
-                } 
-
-                if(primeNumberList) {
-                    
-                    printf("%d ", i);
-                }
-
-            } 
-            printf("\n\n");
-
-            if(isPrimeNumber) {
-                // bei Primzahlen einfach 1 und userInput wieder ausgeben
-                printf("(Primzahl) Deine Primfaktoren sind: 1 und %d \n", userInputNumber);
-            } else {
-                printf("(keine Primzahl) Deine Primfaktoren sind: ");
-
-                // solange durch zwei teilen, bis modulo != 0
-                while (userInputNumber%2 == 0) { 
-                    printf("%d ", 2); 
-                    userInputNumber = userInputNumber/2; 
-                }
-
-                //find i: add to i until 
-
-                for (int divisor = 3; divisor <= userInputNumber; divisor++) { 
-
-                   
-                   //find divisor: check if i is primenumber, if yes
-                   // divide by i until modulo != 0,
-                   //
-                   //
-                   // else add to i and check again
-
-                    //check if counter in loop is prime, and can be used as divisor in prime factorization
-                    bool divisorIsPrime = true;
-                    for (int i = 2; i <= divisor/2; i++) { 
-                        if (divisor % i == 0) { 
-                            divisorIsPrime = false;
-                            break; // Schleife an dieser Stelle beenden
-                        }
-                    }
-                    if(divisorIsPrime) {
-                        while (userInputNumber%divisor == 0) { 
-                            printf("%d ", divisor); 
-                            userInputNumber = userInputNumber/divisor; 
-                        } 
-                    } else {
-                        //divisor is not prime and cannot be used as divisor in primefactorization, therefore continue loop
-                        continue;
-                    }
-
-
-
-                   
-                    
-                } 
+    /*
+    Aufgabe 3: 
+    Prüfen ob Eingabezahl eine Primzahl
+    Ausgabe des Ergebnisses der Prüfung
+    */
+    if (userEingabeZahl <= 1) {
+        istPrimzahl = false; // 1 und 0 sind keine Primzahlen und werden in der weiteren Prüfung vernachlässigt
+    } else {
+        for (int i = 2; i <= userEingabeZahl/2; i++) { 
+            if (userEingabeZahl % i == 0) { 
+                istPrimzahl = false;
+                break; // Schleife an dieser Stelle beenden, Teilen ohne Rest hat einmal funktioniert, Eingabe kann keine Primzahl sein
             }
         }
     }
+
+
+    if(istPrimzahl == true) {
+        printf("Deine Zahl IST eine Primzahl  \n");
+    } else {
+        printf("Deine Zahl ist KEINE Primzahl  \n");
+    }
+    /*
+    Ende Aufgabe 3
+    */
+
+
+
+    //Sicherheitsabfrage, wenn Eingabe > 1000 wird das Programm nicht fortgesetzt.
+    if(userEingabeZahl > 1000) {
+        printf("Zahl muss zum finden der Primfaktoren für deine Zahl kleiner als 1000 sein!\n");
+        return 0;
+    } else {
+        /*
+        Aufgabe 4: 
+        Erstellen einer Liste aller Primzahlen bis zur eingegebenen Zahl
+        */
+        printf("Primzahlen bis zu deiner Zahl:\n");
+        // schleife beginnt bei 1, da 0 keine Primzahl ist, und somit ausgeschlossen werden kann
+        for (int i = 2; i < userEingabeZahl; i++) { 
+
+            // Schleife prüft jede Zahl bis zur Eingabezahl, ob das Teilen durch einen anderen Teiler als 1 oder sich selbst
+            // teilbar ist -> dann keine Primzahl
+            for (int a = 2; a <= i/2; a++) { 
+                if (i % a == 0) { 
+                    primzahlListenElement = false; 
+                    break; 
+                } else {
+                    primzahlListenElement = true; 
+                }
+            } 
+
+            if(primzahlListenElement) {
+                //Zahl als Primzahl bis zur Eingabezahl in selber Iteration ausgeben
+                printf("%d ", i);
+            }
+
+        } 
+        /*
+        Ende Aufgabe 4 
+        */
+
+
+        printf("\n\n"); // nur Formatierung
+
+
+        /*
+        Aufgabe 5: 
+        Primfaktoren zur Eingabezahl finden
+        */
+        if(istPrimzahl) {
+            // bei Primzahlen einfach 1 und Eingabezahl als Teiler wieder ausgeben
+            printf("(Primzahl) Deine Primfaktoren sind: 1 und %d \n", userEingabeZahl);
+        } else {
+            // Primfaktoren ermitteln
+            printf("(keine Primzahl) Deine Primfaktoren sind: ");
+
+            
+            // Alle Zahlen bis zur Eingabezahl iterieren und prüfen, ob es sich dabei um eine Primzahl handelt
+            for (int divisor = 2; divisor <= userEingabeZahl; divisor++) { 
+
+                bool teilerIstPrimzahl = true; // variable hier für jede Schleife neu definieren, damit Standardwert für jede Iteration verwendet wird.
+                
+                // 1 und 0 sind keine Primzahlen und werden in der weiteren Prüfung vernachlässigt
+                for (int i = 2; i <= divisor/2; i++) { 
+                    if (divisor % i == 0) { 
+                        teilerIstPrimzahl = false;
+                        break; // Schleife an dieser Stelle beenden
+                    }
+                }
+                
+                if(teilerIstPrimzahl) {
+                    /*
+                    Ermittelten Primfaktor nutzen und Eingabezahl damit solange teilen bis Rest nicht 0 ist.
+                    Solange ohne Rest geteilt werden kann wird Teiler als Primfaktor ausgegeben.
+                    */
+                    while (userEingabeZahl%divisor == 0) { 
+                        printf("%d ", divisor); 
+                        userEingabeZahl = userEingabeZahl/divisor; 
+                    } 
+                } else {
+                    // Zahl die in dieser Iteration geprüft wurde ist keine Primzahl und wird ignoriert - die Schleife wird fortgesetzt
+                    continue;
+                }
+                
+            } 
+        }
+        /*
+        Ende Aufgabe 5
+        */
+
+
+
+        return 0;
+    }
+    
 }
